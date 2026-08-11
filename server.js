2aa8289717eb55bc526cca48595e6c748d31d769
rovaimsiraj2007-creator <rovaimsiraj2007@gmail.com>
2026-08-11 12:38:45 +0500

    Add products API

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-import express from "axpress";
+import express from "express";
 
 const app = express();
-const PORT = 5000;
+const PORT = 4000;
 
+let products = [];
+
+app.use(express.json());
+
+app.post('/add-product', (req,res)=>{
+    const productBody = req.body
+    products.push(productBody);
+    console.log(products)
+    res.status(201).send({ status: "success", message: "Product Added Successfully" })
+})
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen(PORT, () => {
+    console.log(`App is Running On Port ${PORT}`)
+})
