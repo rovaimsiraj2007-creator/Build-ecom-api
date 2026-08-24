90ad5afbed69ef83dc1482deec578564ddeaeb27
abdullahishtiaq999 <rovaimsiraj2007@gmail.com>
2026-08-24 11:03:32 +0500

    id

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 import express from "express";
 
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
 
 let products = [];
 
 app.use(express.json());
 
-app.post('/add-product', (req,res)=>{
-    const productBody = req.body
+app.post('/add-products', (req,res)=>{
+    const productBody = req.body;
+    if(!productBody?.title || !productBody?.price || !productBody?.description || !productBody?.image ){
+      res.status(400).send({status : 'ERROR', massage : "Required parameter Missing!"})
+      return;
+    }
+    
     products.push(productBody);
     console.log(products)
     res.status(201).send({ status: "success", message: "Product Added Successfully" })
 })
 
 app.get('/products', (req, res) => {
-  res.json(products);
+  res.json({ id: new Date().getTime(), ...products });
 });
 
 app.listen(PORT, () => {
